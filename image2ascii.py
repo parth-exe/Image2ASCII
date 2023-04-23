9499f37ffa47603f460ad7f4c6b1f270b1bb750b
Parth <upadhyaparth420@icloud.com>
2023-04-23 14:26:28 +0530

    Added functions

--- image2ascii.py
+++ image2ascii.py
@@ -2,23 +2,29 @@ import pywhatkit
 from PIL import Image
 from FILEPATH import input_path
 # FILE PATH
-file_name = input("Enter file name with extension: ")
-final_path = input_path + file_name
+def get_filepath():
+    file_name = input("Enter file name with extension: ")
+    final_path = input_path + file_name
+    return final_path
 # Opening Image
-try:
-    inp = Image.open(final_path)
-# Converting to ASCII
-    pywhatkit.image_to_ascii_art(final_path,'Image2Ascii/final') 
-except Exception as e:
-    #\033[1;31m for Red Color
-    #\033[1;32m for Green Color
-    print(f"\033[1;31m [!]Exception {e} encountered[!]")
-    print("\033[1;32m Exiting program, run again")
-    quit()
+def open_image(final_path):
+    try:
+        inp = Image.open(final_path)
+    # Converting to ASCII
+        pywhatkit.image_to_ascii_art(final_path,'Image2ASCII/final') 
+    except Exception as e:
+        #\033[1;31m for Red Color
+        #\033[1;32m for Green Color
+        print(f"\033[1;31m [!]Exception {e} encountered[!]")
+        print("\033[1;32m Exiting program, run again")
+        quit()
+    read_file = open("Image2ASCII/final.txt","r")
+    # Reading File
+    print(read_file.read())
+    read_file.close()
 
-read_file = open("Image2Ascii/final.txt","r")
-# Reading File
-print(read_file.read())
-read_file.close()
+def main():
+    open_image(get_filepath())
 
-
+if __name__ == "__main__":
+    main()
